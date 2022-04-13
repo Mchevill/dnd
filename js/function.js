006f4aa6969bef2982a0650f9ca5a3aa011dead9
Mchevill <66390249+Mchevill@users.noreply.github.com>
2022-04-13 15:53:35 -0300

    Add files via upload
    Primer progreso app dnd

---
+++ js/function.js
@@ -0,0 +1,23 @@
+function statsOn(){
+	x = document.getElementById('visible');
+	x.style.visibility = "visible";
+	y = document.getElementById('menujs');
+	y.style.top = "380px";
+}
+
+function statsOff(){
+	x = document.getElementById('visible');
+	x.style.visibility = "hidden";
+	y = document.getElementById('menujs');
+	y.style.top = "558px";
+}
+
+function statsOnOff(){
+	z = document.getElementById('visible');
+
+	if (z.style.visibility == 'hidden'){
+		statsOn();
+	} else {
+		statsOff();
+	}
+}
